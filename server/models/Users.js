74a8895b2fcc911261848de8822c405216a531c1
Elie uwimana <muhizielie01@gmail.com>
2021-12-29 09:32:54 +0200

    add: users feature

---
+++ server/models/Users.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    userId: 1,
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john@gmail.com',
+    password: '$2b$10$9XHI72tbOtjfiZpUzlG0FOvHhwf.yL/JIs0aMvZpwh0oY/oTS0C3e',
+  },
+  {
+    userId: 2,
+    firstName: 'Jane',
+    lastName: 'Doe',
+    email: 'jane@gmail.com',
+    password: '$2b$10$0cAXA8TWPQVaZGq3We0peuvpTDJO.B68DlHskE5EuBhV2kZkEh6Xi',
+  },
+
+];
+export default users;
