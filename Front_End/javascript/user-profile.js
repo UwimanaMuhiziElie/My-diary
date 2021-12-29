c67782f01fc86c3125a2db6f17b229280052e1ac
Elie uwimana <muhizielie01@gmail.com>
2021-12-29 09:18:41 +0200

    add: user profile functionality

---
+++ Front_End/javascript/user-profile.js
@@ -0,0 +1,9 @@
+function toggleSidebar() {
+    document.getElementById('sidebar').classList.toggle('active');
+}
+
+// NOTIFICATION DROPDOWN
+  
+function dropDown() {
+    document.getElementById('notificationDrop').classList.toggle('show');
+  }
