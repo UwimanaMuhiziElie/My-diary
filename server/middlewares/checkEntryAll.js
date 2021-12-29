8372793aaae40d253eddeeeba4b89d3a6a996b6e
Elie uwimana <muhizielie01@gmail.com>
2021-12-29 09:48:13 +0200

    add:check all entries validation

---
+++ server/middlewares/checkEntryAll.js
@@ -0,0 +1,14 @@
+import entries from '../models/Entries';
+import Responsender from '../helpers/responseHandler';
+import { NOT_FOUND_STATUS_CODE } from '../helpers/statusCodeHandler';
+
+export default async (req, res, next) => {
+  const response = new Responsender();
+  const entry = entries.filter((c) => c.userId === req.userData.userId);
+
+  if (entry.length === 0) {
+    response.error(NOT_FOUND_STATUS_CODE, 'You do not have any Entry yet, create an entry and try again');
+    return response.send(res);
+  }
+  next();
+};
