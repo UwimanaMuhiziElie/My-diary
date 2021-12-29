7d38ac63d8a2f6be9887b024d234b1e7a8294d25
Elie uwimana <muhizielie01@gmail.com>
2021-12-29 09:31:46 +0200

    add:user entries functionality

---
+++ server/models/Entries.js
@@ -0,0 +1,10 @@
+const entries = [
+  {
+    id: 1,
+    userId: 1,
+    createdOn: 'Mon Oct 28 2019',
+    title: 'Nice day',
+    description: 'jdadkhjkaldhldald',
+  },
+];
+export default entries;
