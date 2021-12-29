9581ac037894b05945dc671c0e584d9ddae617cf
Elie uwimana <muhizielie01@gmail.com>
2021-12-29 09:17:09 +0200

    add: main user functionality

---
+++ Front_End/javascript/main.js
@@ -0,0 +1,6 @@
+function toggleSidebar() {
+  document.getElementById('sidebar').classList.toggle('active');
+}
+function dropDown() {
+  document.getElementById('notificationDrop').classList.toggle('show');
+}
